ad21a0c6310be40f7e7c29e013b2e60fce886534
Yaroslav <93050088+ZeedKey@users.noreply.github.com>
2022-06-26 21:52:13 +0400

    Update index.tsx

--- src/components/constraints/CartItem/index.tsx
+++ src/components/constraints/CartItem/index.tsx
@@ -41,10 +41,9 @@ export const CartItem: React.FC<IProduct> = (props) => {
   const { cart, updateCart, setSum } = useCart();
   useEffect(() => {
     updateCart({ ...props, amount: amount });
-    const sum: number = cart?
-      .map((e) => {
+    const sum: number = (cart !== undefined ? cart.map((e) => {
      return e !== undefined? e?.price * e?.amount : 0;
-    })
+    }) : [])
       .reduce((prev, next) => prev + next);
     setSum(sum);
   });
